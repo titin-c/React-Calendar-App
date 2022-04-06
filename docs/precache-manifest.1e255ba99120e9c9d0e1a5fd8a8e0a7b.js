fbe14129e64ea917fa25591475957cf5ef033ede
Your NameTitín <titin.colomer.romero@gmail.com>
2022-04-06 13:38:24 +0200

    arreglado ruta con el modulo gh-pages y docs en lugar de build

---
+++ docs/precache-manifest.1e255ba99120e9c9d0e1a5fd8a8e0a7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf7b95ce3263b1616ee159922d25332",
+    "url": "/React-Calendar-App/index.html"
+  },
+  {
+    "revision": "7694426fd22d0b8aa50c",
+    "url": "/React-Calendar-App/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "8ed40c246e5cd1b82bc1",
+    "url": "/React-Calendar-App/static/css/main.62859fb1.chunk.css"
+  },
+  {
+    "revision": "7694426fd22d0b8aa50c",
+    "url": "/React-Calendar-App/static/js/2.50e5c216.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/React-Calendar-App/static/js/2.50e5c216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed40c246e5cd1b82bc1",
+    "url": "/React-Calendar-App/static/js/main.1a02650d.chunk.js"
+  },
+  {
+    "revision": "a3232282dbceb26a35ac",
+    "url": "/React-Calendar-App/static/js/runtime-main.4b42c1a6.js"
+  }
+]);
